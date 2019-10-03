4c2cefe2a80b1bedee0c800554008814bd9631f5
Nicole Roberts <nicolerobertsb@gmail.com>
2019-10-02 18:03:00 -0700

    added products to the table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,16 @@ CREATE TABLE products(
   stock_quantity INT NOT NULL DEFAULT '1',
   PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
+VALUES ("MacBook Pro Laptop", "Electronics", 1399.99, 10),
+("Wine Glasses", "Kitchen", 34.99, 30),
+("Dog Bed", "Pet", 109.99, 50),
+("Pearl Earrings", "Jewelery", 299.99, 17),
+("Leather Gloves", "Accessories", 68.99, 22),
+("Pogo Stick", "Toys", 30.00, 200),
+("Telescope", "Outdoors", 129.99, 50),
+("Large Round Mirror", "Home", 99.99, 39),
+("Birthday Cake Scented Candle", "Home", 12.99, 346),
+("Wool Scarf", "Accessories", 72.99, 6),
